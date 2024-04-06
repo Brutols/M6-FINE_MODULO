ceb675c3e2ae30145c72176be3ddd312dd5d694d
Brutols <dtwo97@gmail.com>
2024-04-06 12:05:39 +0200

    homePage styling

--- view/src/pages/HomePage.jsx
+++ view/src/pages/HomePage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../components/card/Card'
+import axios from "axios"
+import styles from "./homePage.module.css"
 
 const HomePage = () => {
+  const [travels, setTravels] = useState()
+
+  const getTravels = async () => {
+    try {
+      const res = await axios.get("http://localhost:3030/travels")
+      setTravels(res.data.travels)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  
+  useEffect(() => {
+    getTravels()
+  }, [])
+
   return (
-    <div>HomePage</div>
+    <div className={styles.cardWrapper} >
+      {travels && travels.map((travel, i) => {
+        return <Card key={i} location={travel.location} img={travel.img} description={travel.description} price={travel.price} />
+      })}
+    </div>
   )
 }
 
