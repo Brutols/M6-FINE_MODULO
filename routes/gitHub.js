14a395e4af4f76fc3b173ee9affa8dd0be2bcdc0
Brutols <dtwo97@gmail.com>
2024-04-08 21:20:55 +0200

    fix 4

--- routes/gitHub.js
+++ routes/gitHub.js
@@ -3,7 +3,7 @@ const gitHub = express.Router();
 const strategy = require("passport-github").Strategy;
 const passport = require("passport");
 require("dotenv").config();
-const session = require("cookie-session");
+const session = require("express-session");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel")
 const crypto = require("crypto")
